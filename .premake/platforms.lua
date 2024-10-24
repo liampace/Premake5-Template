33d85000a3a5d6018d937f57e3dcf0d2a59a7e7b
Liam Pace <146035497+liampace@users.noreply.github.com>
2024-10-24 11:18:53 -0400

    configurable multi platform support

---
+++ .premake/platforms.lua
@@ -0,0 +1,5 @@
+PLT_SYSTEMS = {
+	["windows"] = {"x86", "x86_64"},
+	["linus"] = {"ARM", "ARM64"},
+	["macosx"] = {"universal"}
+}
--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,23 @@
+include ".premake/project.lua"
+
 if not pcall(workspace) then 
+	include ".premake/platforms.lua"
 	workspace "Workspace"
 		configurations {"Test", "Development", "Profile", "Release"}
-		architecture "x86_64"
-		system "windows"
+		for os, arches in pairs(PLT_SYSTEMS) do
+			filter("system:"..os)
+				platforms(arches)
+				for _, arch in ipairs(arches) do
+					filter {"system:"..os, "platforms:"..arch}
+						architecture(arch)
+				end
+		end
 		systemversion "latest"
-		startproject "Demo"
+		startproject(PRJ_NAME)
 
 	BUILD_DIR = "%{prj.location}/build/"
 	OUTPUT_DIR = "%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}/"
 end
-
-include ".premake/project.lua"
 
 project(PRJ_NAME)
 	kind(PRJ_KIND)
